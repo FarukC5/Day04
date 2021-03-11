36c50df6fbb450caea97fdf5c6f5f2ae44ae8051
FarukC5 <faruk_cajic@yahoo.com>
2021-03-12 00:54:01 +0100

    added

---
+++ ex10/src/index.js
@@ -0,0 +1,19 @@
+function main(){
+
+    var myFirstName = "Faruk";
+    var myLastName = "Cajic";
+    var myHomeTown = "Doboj";
+
+return {
+
+    myFirstName,
+    myLastName,
+    myHomeTown
+
+  };
+
+
+}
+
+console.log(main());
+module.exports = main;
